a7fdd4792e14c2b488669b6708ee30df1b39d958
abcdifg <jura.samohin@gmail.com>
2024-08-02 21:55:43 +0300

    small change to viewport

--- src/viewport.rs
+++ src/viewport.rs
@@ -15,6 +15,13 @@ pub fn spawn_viewport(commands: &mut Commands, settings: &Res<EditorSettings>) -
                     align_items: AlignItems::Center,
                     ..default()
                 },
+                
+                border_radius: BorderRadius::new (
+                    Val::Px(4.8),
+                    Val::Px(4.8),
+                    Val::Px(4.8),
+                    Val::Px(4.8),
+                ),
                 background_color: Color::srgb(0.1, 0.1, 0.1).into(),
                 ..default()
             },
