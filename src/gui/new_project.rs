b97f92f16dcae9b8689e0aee31c1937928d475c1
abcdifg <jura.samohin@gmail.com>
2024-08-09 00:45:43 +0300

    added basic ui canvas for project manager and project creator

--- src/gui/new_project.rs
+++ src/gui/new_project.rs
@@ -1,7 +1,14 @@
 use bevy::{prelude::*, render::camera::RenderTarget, window::WindowRef};
+use crate::editor_settings::EditorSettings;
 use crate::gui::ui_components::FileButtonsAction;
 use crate::editor::create_project;
 use crate::gui::project_manager;
+
+#[derive(Component)]
+pub struct ProjectCreator;
+
+#[derive(Component)]
+pub struct ProjectCreatorCamera;
 
 pub fn open_project_creator_system(
     mut commands: Commands,
@@ -10,23 +17,54 @@ pub fn open_project_creator_system(
 ) {
     for (interaction, action) in interaction_query.iter() {
         if *interaction == Interaction::Pressed && matches!(action, FileButtonsAction::New) {
-            let project_selector_window = commands
+            let project_creator_window = commands
                 .spawn(Window {
                     title: "Create new project".to_owned(),
                     ..default()
                 })
                 .id();
             
-            let second_window_camera = commands
-                .spawn(Camera2dBundle {
+            let project_creator_camera = commands
+            .spawn((
+                Camera2dBundle {
                     camera: Camera {
-                        target: RenderTarget::Window(WindowRef::Entity(project_selector_window)),
+                        target: RenderTarget::Window(WindowRef::Entity(project_creator_window)),
                         ..default()
                     },
                     ..default()
-                })
-                .id();
+                },
+                ProjectCreatorCamera
+            ))
+            .id();
 
         }
     }
 }
+
+pub fn setup_ui(
+    mut commands: Commands, 
+    asset_server: Res<AssetServer>, 
+    settings: Res<EditorSettings>,
+    camera_query: Query<Entity, With<ProjectCreatorCamera>>,) {
+        if let Ok(project_manager_camera) = camera_query.get_single() {
+            commands.spawn((
+                NodeBundle {
+                    style: Style {
+                        width: Val::Percent(100.0),
+                        height: Val::Percent(100.0),
+                        display: Display::Grid,
+                        grid_template_columns: vec![GridTrack::fr(1.0), GridTrack::fr(3.0), GridTrack::fr(1.0)],
+                        grid_template_rows: vec![GridTrack::px(50.0), GridTrack::fr(1.0), GridTrack::px(160.0)],
+                        column_gap: Val::Px(5.0),
+                        row_gap: Val::Px(5.0),
+                        padding: UiRect::all(Val::Px(5.0)),
+                        ..default()
+                    },
+                    background_color: settings.sub_panel_background.into(),
+                    ..default()
+                },
+                ProjectCreator,
+                TargetCamera(project_manager_camera),
+            ));
+        }
+    }
--- src/gui/project_manager.rs
+++ src/gui/project_manager.rs
@@ -1,10 +1,14 @@
 use bevy::{prelude::*, render::camera::RenderTarget, window::WindowRef};
+use crate::editor_settings::EditorSettings;
 use crate::gui::ui_components::FileButtonsAction;
 use crate::editor::create_project;
 use crate::gui::project_manager;
 
 #[derive(Component)]
-pub struct ProjectSelectorWindowVisible(bool);
+pub struct ProjectSelector;
+
+#[derive(Component)]
+pub struct ProjectManagerCamera;
 
 pub fn open_project_selector_system(
     mut commands: Commands,
@@ -20,16 +24,47 @@ pub fn open_project_selector_system(
                 })
                 .id();
             
-            let second_window_camera = commands
-                .spawn(Camera2dBundle {
-                    camera: Camera {
-                        target: RenderTarget::Window(WindowRef::Entity(project_selector_window)),
+                let project_manager_camera = commands
+                .spawn((
+                    Camera2dBundle {
+                        camera: Camera {
+                            target: RenderTarget::Window(WindowRef::Entity(project_selector_window)),
+                            ..default()
+                        },
                         ..default()
                     },
-                    ..default()
-                })
+                    ProjectManagerCamera
+                ))
                 .id();
 
         }
     }
 }
+
+pub fn setup_ui(
+    mut commands: Commands, 
+    asset_server: Res<AssetServer>, 
+    settings: Res<EditorSettings>,
+    camera_query: Query<Entity, With<ProjectManagerCamera>>,) {
+        if let Ok(project_manager_camera) = camera_query.get_single() {
+            commands.spawn((
+                NodeBundle {
+                    style: Style {
+                        width: Val::Percent(100.0),
+                        height: Val::Percent(100.0),
+                        display: Display::Grid,
+                        grid_template_columns: vec![GridTrack::fr(1.0), GridTrack::fr(3.0), GridTrack::fr(1.0)],
+                        grid_template_rows: vec![GridTrack::px(50.0), GridTrack::fr(1.0), GridTrack::px(160.0)],
+                        column_gap: Val::Px(5.0),
+                        row_gap: Val::Px(5.0),
+                        padding: UiRect::all(Val::Px(5.0)),
+                        ..default()
+                    },
+                    background_color: settings.sub_panel_background.into(),
+                    ..default()
+                },
+                ProjectSelector,
+                TargetCamera(project_manager_camera),
+            ));
+        }
+    }
