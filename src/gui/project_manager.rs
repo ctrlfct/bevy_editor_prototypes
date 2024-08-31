e5bd4eb7230e78c59e7f409fb756e9b283145672
abcdifg <jura.samohin@gmail.com>
2024-08-31 09:56:18 +0300

    added some basic ui for project_manager

--- src/gui/project_manager.rs
+++ src/gui/project_manager.rs
@@ -47,16 +47,18 @@ pub fn setup_ui(
     mut commands: Commands, 
     asset_server: Res<AssetServer>, 
     settings: Res<EditorSettings>,
-    camera_query: Query<Entity, With<ProjectManagerCamera>>,) {
-        if let Ok(project_manager_camera) = camera_query.get_single() {
-            commands.spawn((
+    camera_query: Query<Entity, With<ProjectManagerCamera>>,
+) {
+    if let Ok(project_manager_camera) = camera_query.get_single() {
+        let canvas_entity = commands
+            .spawn((
                 NodeBundle {
                     style: Style {
                         width: Val::Percent(100.0),
                         height: Val::Percent(100.0),
                         display: Display::Grid,
-                        grid_template_columns: vec![GridTrack::fr(1.0), GridTrack::fr(3.0), GridTrack::fr(1.0)],
-                        grid_template_rows: vec![GridTrack::px(50.0), GridTrack::fr(1.0), GridTrack::px(160.0)],
+                        grid_template_columns: vec![GridTrack::fr(2.0), GridTrack::fr(1.0)],
+                        grid_template_rows: vec![GridTrack::px(50.0), GridTrack::fr(1.0)],
                         column_gap: Val::Px(5.0),
                         row_gap: Val::Px(5.0),
                         padding: UiRect::all(Val::Px(5.0)),
@@ -67,6 +69,81 @@ pub fn setup_ui(
                 },
                 ProjectSelector,
                 TargetCamera(project_manager_camera),
-            ));
-        }
+            ))
+            .id();
+
+        let top_panel = commands
+            .spawn(NodeBundle {
+                style: Style {
+                    width: Val::Percent(100.0),
+                    height: Val::Percent(100.0),
+                    ..default()
+                },
+                border_radius: BorderRadius::new (
+                    Val::Px(4.8),
+                    Val::Px(4.8),
+                    Val::Px(4.8),
+                    Val::Px(4.8),
+                ),
+                background_color: settings.panel_background.into(),
+                ..default()
+            })
+            .id();
+
+        let left_panel = commands
+            .spawn(NodeBundle {
+                style: Style {
+                    width: Val::Percent(100.0),
+                    height: Val::Percent(100.0),
+                    ..default()
+                },
+                border_radius: BorderRadius::new (
+                    Val::Px(4.8),
+                    Val::Px(4.8),
+                    Val::Px(4.8),
+                    Val::Px(4.8),
+                ),
+                background_color: settings.panel_background.into(),
+                ..default()
+            })
+            .id();
+
+        let right_panel = commands
+            .spawn(NodeBundle {
+                style: Style {
+                    width: Val::Percent(100.0),
+                    height: Val::Percent(100.0),
+                    ..default()
+                },
+                border_radius: BorderRadius::new (
+                    Val::Px(4.8),
+                    Val::Px(4.8),
+                    Val::Px(4.8),
+                    Val::Px(4.8),
+                ),
+                background_color: settings.panel_background.into(),
+                ..default()
+            })
+            .id();
+
+        commands.entity(canvas_entity).push_children(&[top_panel, left_panel, right_panel]);
+
+        commands.entity(top_panel).insert(Style {
+            grid_column: GridPlacement::span(2),
+            grid_row: GridPlacement::start(1),
+            ..default()
+        });
+
+        commands.entity(left_panel).insert(Style {
+            grid_column: GridPlacement::start(1),
+            grid_row: GridPlacement::start(2),
+            ..default()
+        });
+
+        commands.entity(right_panel).insert(Style {
+            grid_column: GridPlacement::start(2),
+            grid_row: GridPlacement::start(2),
+            ..default()
+        });
     }
+}
